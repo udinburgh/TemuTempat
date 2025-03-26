4ac528371cd4eabf873758a51f9b08dc6233880a
Cynthiashabrn <cynthiashabrina87@gmail.com>
2025-03-26 14:48:34 +0700

    Y

--- TemuTempat/ContentView.swift
+++ TemuTempat/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+//        Test Push
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
